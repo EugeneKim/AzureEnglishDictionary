a7cb51ba0df9c5dc11db13279f750e34f6f0ad53
Eugene Kim <eugene.kimley@gmail.com>
2022-05-15 10:10:51 +1000

    Added the model.

---
+++ ServiceModels/WordItem.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace ServiceModels
+{
+	#region Definitions
+
+	public record WordItem(
+		[property: JsonProperty("id")]
+		string Id,
+		[property: JsonProperty("meanings")]
+		List<MeaningItem> Meanings);
+	public record MeaningItem(
+		[property: JsonProperty("partOfSpeech")]
+		string PartOfSpeech,
+		[property: JsonProperty("definitions")]
+		List<DefinitionItem> Definitions);
+	public record DefinitionItem(
+		[property: JsonProperty("definition")]
+		string Definition,
+		[property: JsonProperty("synonyms")]
+		List<string> Synonyms,
+		[property: JsonProperty("antonyms")]
+		List<string> Antonyms,
+		[property: JsonProperty("example")]
+		string Example);
+
+	#endregion
+}
